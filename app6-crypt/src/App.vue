2703e13359d11ae6fc8a083d114853b37d8cd37c
trenersambo <arb.pfr@gmail.com>
2022-07-03 10:45:28 +0300

    Клик на ПЛИТКУ:появился ГРАФИК

--- app6-crypt/src/App.vue
+++ app6-crypt/src/App.vue
@@ -1,7 +1,8 @@
 <template>
 
   <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
-  <div class=" w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center">
+  <div class=" w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex 
+  items-center justify-center">
     <!-- <svg class="animate-spin -ml-1 mr-3 h-12 w-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
       <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
       <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
@@ -30,16 +31,20 @@
             />
           </div>
           <div class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">
-            <span class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+            <span class="inline-flex items-center px-2 m-1 rounded-md text-xs 
+            font-medium bg-gray-300 text-gray-800 cursor-pointer">
               BTC
             </span>
-            <span class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+            <span class="inline-flex items-center px-2 m-1 rounded-md text-xs 
+            ont-medium bg-gray-300 text-gray-800 cursor-pointer">
               DOGE
             </span>
-            <span class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+            <span class="inline-flex items-center px-2 m-1 rounded-md text-xs 
+            font-medium bg-gray-300 text-gray-800 cursor-pointer">
               BCH
             </span>
-            <span class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+            <span class="inline-flex items-center px-2 m-1 rounded-md text-xs 
+            font-medium bg-gray-300 text-gray-800 cursor-pointer">
               CHD
             </span>
           </div>
@@ -49,7 +54,10 @@
       <button
         @click="add()"
         type="button"
-        class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+        class="my-4 inline-flex items-center py-2 px-4 border border-transparent 
+        shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 
+        hover:bg-gray-700 transition-colors duration-300 focus:outline-none 
+        focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
       >
         <!-- Heroicon name: solid/mail -->
         <svg
@@ -69,38 +77,47 @@
     </section>
 
       <hr class="w-full border-t border-gray-600 my-4" />
+            Тест клика на ПЛАШКУ:{{sel}}
       <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-<div
-  v-for="(tick,index) in tickers" :key="index"
-  class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
->
-  <div class="px-4 py-5 sm:p-6 text-center">
-    <dt class="text-sm font-medium text-gray-500 truncate">
-      Индекс: {{index}} // {{tick.name}}  - USD    
-    </dt>
-    <dd class="mt-1 text-3xl font-semibold text-gray-900">
-      {{tick.price}}
-    </dd>
-  </div>
-  <div class="w-full border-t border-gray-200"></div>
-  <button
-    @click="removeTicket(tick, index)"
-    class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
-  >
-    <svg
-      class="h-5 w-5"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 20 20"
-      fill="#718096"
-      aria-hidden="true"
+
+    <div
+      v-for="(tick,index) in tickers" :key="index"
+      @click="sel = tick"
+
+
+      class="bg-white overflow-hidden shadow rounded-lg 
+      border-purple-800 border-solid cursor-pointer"
     >
-      <path
-        fill-rule="evenodd"
-        d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-        clip-rule="evenodd"
-      ></path></svg>Удалить
-  </button>
-</div>
+      <div class="px-4 py-5 sm:p-6 text-center">
+        <dt class="text-sm font-medium text-gray-500 truncate">
+          Индекс: {{index}} // {{tick.name}}  - USD    
+        </dt>
+        <dd class="mt-1 text-3xl font-semibold text-gray-900">
+          {{tick.price}}
+        </dd>
+      </div>
+      <div class="w-full border-t border-gray-200"></div>
+      <button
+        @click="removeTicket(tick, index)"
+        class="flex items-center justify-center font-medium 
+        w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500
+        hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all 
+        focus:outline-none"
+      >
+        <svg
+          class="h-5 w-5"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="#718096"
+          aria-hidden="true"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+            clip-rule="evenodd"
+          ></path></svg>Удалить
+      </button>
+    </div>
         <!-- <div
           class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid border-4 cursor-pointer"
         >
@@ -192,7 +209,8 @@
       <hr 
       v-if="tickers.length>0"
       class="w-full border-t border-gray-600 my-4" />
-    <section class="relative">
+
+    <section v-if="sel" class="relative">
       <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
         VUE - USD
       </h3>
@@ -250,6 +268,9 @@ export default {
       {name:"Эфириум" , price: "1054"},
       {name:"Догкоин", price: "$ 0.065"},
       ],
+
+      sel: null,
+
     };
 
   },
