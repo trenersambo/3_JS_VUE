f8f967b2a57bfa28d180e6c8aecd77effb4e45f3
trenersambo <arb.pfr@gmail.com>
2022-07-31 11:08:31 +0300

    Кнопки Вперед/Назад/Фильтр

--- app6-crypt/src/App.vue
+++ app6-crypt/src/App.vue
@@ -80,6 +80,24 @@
     </section>
 
       <hr class="w-full border-t border-gray-600 my-4" />
+  <div>
+    <button 
+    class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent 
+        shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 
+        hover:bg-gray-700 transition-colors duration-300 focus:outline-none 
+        focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+    >Назад</button>
+
+    <button
+    class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent 
+        shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 
+        hover:bg-gray-700 transition-colors duration-300 focus:outline-none 
+        focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+    >Вперед</button> 
+    
+    <div>Фильтр: <input /> </div>
+  </div>
+
             Тест клика на ПЛАШКУ:{{sel}}
       <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
 
